302594464cfefd212586061ab91baa1492864509
HenryCodeT <120686@unsaac.edu.pe>
2022-01-10 14:35:06 -0500

    add muted

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 console.log("page loaded...");
 function over(event) {
-    console.log("event over",event);
-    console.log(event.play());
     event.play()
+    event.muted ="false"
 }
 function out(event) {
-    console.log("event out",event);
     event.pause()
+    event.muted ="true"
 }
